5c53aca0f607742911f01e79fe7a2f04731b3adc
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-09-22 03:13:35 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-if
+const { Client, Partials } = require("discord.js");
+
+const { User, Message, GuildMember, ThreadMember } = Partials;
+
+const client = new Client({
+  intents: [],
+  partials: [User, Message, GuildMember, ThreadMember]
+});
+
+client.config = require("./config.json");
+
+client.on("ready", () => { 
+  console.log("el bot se inicio correctamente");
+});
+ 
+client.login(client.config.token);
